6b80fb49aeaf719e6491bdb3d9887b93f5152e8c
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2017-08-22 15:15:24 +0530

    vowel.c

---
+++ vowel.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char inp,ch;
+printf("enter the variable");
+scanf("%c",&inp);
+switch(ch)
+{
+case 'a':
+printf("/n vowel");
+break;
+case 'e':
+printf("/n vowel");
+break;
+case 'i':
+printf("/n vowel");
+break;
+case 'o':
+printf("/n vowel");
+break;
+case 'u':
+printf("\n vowel");
+break;
+case 'A':
+printf("\n vowel");
+break;
+case 'E':
+printf("\n vowel");
+break;
+case 'I':
+printf("\n vowel");
+break;
+case 'O':
+printf("\n vowel");
+break;
+case 'U':
+printf("\n vowel");
+break;
+default:
+printf("constant");
+}
+getch();
+}
