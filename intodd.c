d435b259507dca54c62cdc4dce9c573415e138f4
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2017-08-29 16:58:43 +0530

    Create intodd.c

---
+++ intodd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int m,n;
+printf("enter the start digit:");
+scanf("%d",&m);
+printf("enter the end digit:");
+scanf("%d",&n);
+printf("the odd numbers are:");
+for(i=m;i<=n;i++)
+{
+if(n%2!=0)
+{
+printf();
