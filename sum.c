0686943acfff1cab96fdc1ca6319dba6351bd2af
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2017-08-29 16:08:17 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=1,sum=0,n;
+printf("enter the total value");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("the sum is %d",sum);
+getch();
+}
