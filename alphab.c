9e3dd07ac98f702b5d811e2d289cce2a4b570019
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2017-08-29 15:58:54 +0530

    Rename alpha.c to alphab.c

---
+++ alphab.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("enter the character"):
+scanf("%c",&n);
+if(n='a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y,'z');
+{
+printf("this is character");
+}
+else
+{
+printf("it is number");
+}
+getch();
+}
