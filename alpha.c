8821208cac6546260d81d4f79b3618e533ac93e9
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2017-08-29 15:53:12 +0530

    alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("enter the character");
+scanf("%c",&n);
+if(n='a','b','c','d','e','f','g','h','g','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+{
+printf("it is a character");
+}
+else
+{
+printf("it is a number");
+}
+}
