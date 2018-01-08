82ad206784074e118d43d4aa77ae1b306b1c7070
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2018-01-08 16:31:07 +0530

    prime.c

---
+++ prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the number:");
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{
+if(n%i==0)
+{
+printf("%d is a prime number,n);
+}
+else
+{
+printf("5d is not a prime number,n);
+}
+return 0;
+}
