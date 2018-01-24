4f818656f0c3177a9d243b11321db628c3b63d4c
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2018-01-24 15:54:05 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a[50],i,j,n;
+printf("enter the size:");
+scanf("%d",&n);
+printf("enter the elements:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i;j<=i;j++)
+{
+printf("%d\t %d\n");
+}
+return 0;
+}
