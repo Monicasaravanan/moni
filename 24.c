ac88335c8367b4e151811ec142c37dc8f8267fb5
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2018-01-31 16:28:29 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int i,j,m,n,a[5],t;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+m=n-2;
+{
+printf("%d",a[m]);
+}
+return 0;
+}
