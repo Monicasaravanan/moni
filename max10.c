36d367ce545e1ab2aaa0f630b33ad78777381868
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2018-02-02 16:30:25 +0530

    Create max10.c

---
+++ max10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,a[10],n;
+int max;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]>max)
+max=a[i];
+}
+printf("%d",max);
+return 0;
+}
