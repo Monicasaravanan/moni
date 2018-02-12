5de593585f72e0e092678e1f1ee527a566a7290a
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2018-02-12 15:49:05 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+switch(n)
+{
+case 1:
+printf("\none");
+break;
+case 2:
+printf("\ntwo");
+break;
+case 3:
+printf("\nthree");
+break;
+case 4:
+printf("\nfour");
+break;
+case 5:
+printf("\nfive");
+break;
+case 6:
+printf("\nsix");
+break;
+case 7:
+printf("\nseven");
+break;
+case 8:
+printf("\neight");
+break;
+case 9:
+printf("\nnine");
+break;
+case 10:
+printf("\nten");
+break;
+default:
+printf("\n invalid");
+}
+return 0;
+}
+
+
+
+
+
+
+
