31e600626622ad83e6c2db91ed79ec8c4633d715
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2018-03-12 16:04:45 +0530

    Create 96*.c

---
+++ 96*.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int n,i,flag=0;
+scanf("%d",&n);
+for(i=2;i<n/2;i++)
+{
+if(n%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag=1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
