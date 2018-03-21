0be5720bb7003f9295b8fea0a5414a7dbffc7784
Monicasaravanan <30549720+Monicasaravanan@users.noreply.github.com>
2018-03-21 15:50:22 +0530

    Update 103.c

--- 103.c
+++ 103.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-int n,i=0;
+int i;
 char s[50];
 scanf("%[^\n]s",s);
-n=strlen(s)
-for(i=0;str[i]!='\0';i++)
+for(i=0;s[i]!='\0';i++)
 {
 if(i==0)
+{
 s[i]=toupper(s[i]);
-if(str[i]=='')
+}
+if(s[i]==' ')
+{
 s[i+1]=toupper(s[i+1]);
+}
 }
 printf("%s",s);
 return 0;
